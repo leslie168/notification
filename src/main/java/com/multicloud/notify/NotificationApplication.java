287c3211b3c7f094df052160808358fc98d2945c
leslie <leslie@lesliedembp>
2017-05-06 13:28:22 +0800

    change apilcation

--- src/main/java/com/multicloud/notify/NotificationApplication.java
+++ src/main/java/com/multicloud/notify/NotificationApplication.java
@@ -4,14 +4,21 @@ import java.util.Date;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class NotificationApplication {
+public class NotificationApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
@@ -20,7 +27,10 @@ public class NotificationApplication {
 	
 	}
 	
-	
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(NotificationApplication.class);
+	    }
 	
 	
 
