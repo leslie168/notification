6719dc5162c177e7f683b02cdb95b1d4f92afb34
leslie <leslie@lesliedembp>
2017-05-06 14:08:30 +0800

    update app

--- src/main/java/com/multicloud/notify/NotificationApplication.java
+++ src/main/java/com/multicloud/notify/NotificationApplication.java
@@ -4,21 +4,14 @@ import java.util.Date;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
-public class NotificationApplication extends SpringBootServletInitializer{
+public class NotificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
@@ -27,10 +20,7 @@ public class NotificationApplication extends SpringBootServletInitializer{
 	
 	}
 	
-	  @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        return application.sources(NotificationApplication.class);
-	    }
+	
 	
 	
 
